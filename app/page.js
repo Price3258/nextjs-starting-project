03833e08157a7bac97c6a5593373d72812bd5712
김원태 <rladnjsxo123@naver.com>
2025-02-27 15:35:20 +0900

    92 페이지 및 레이아웃 작업하기

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 import Link from "next/link";
+import Headers from "@/components/header";
 
 export default function Home() {
   return (
     <main>
-      <img src="/logo.png" alt="A server surrounded by magic sparkles." />
-      <h1>Welcome to this NextJS Course!</h1>
+      <Headers />
       <p>🔥 Let&apos;s get started! 🔥</p>
       <p>
         <Link href="/about">Go About</Link>
