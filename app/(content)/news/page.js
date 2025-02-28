65b479fa014f318bd4bf4fe26e65afd495f6a068
김원태 <rladnjsxo123@naver.com>
2025-02-28 20:11:48 +0900

    161 클라이언트 측 데이터 가져오기

--- app/(content)/news/page.js
+++ app/(content)/news/page.js
@@ -1,11 +1,40 @@
-import { DUMMY_NEWS } from '@/dummy-news';
-import NewsList from '@/components/news-list';
+"use client";
+
+import NewsList from "@/components/news-list";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      setIsLoading(true);
+      const response = await fetch("http://localhost:8080/news");
+      if (!response.ok) {
+        setError("Something went wrong!");
+        setIsLoading(false);
+      }
+      const data = await response.json();
+      setIsLoading(false);
+      setNews(data);
+    }
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {news && <NewsList news={news} />}
     </>
   );
 }
