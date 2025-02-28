a1a8c1b11860e777e1d895a2458386b4437606d2
김원태 <rladnjsxo123@naver.com>
2025-02-28 18:14:31 +0900

    147 에러 추가

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -33,6 +33,14 @@ export default async function FilteredNewsPage({ params }) {
     newsContent = <NewsList news={news} />;
   }
 
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsMonths(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter year");
+  }
+
   return (
     <>
       <header id="archive-header">
